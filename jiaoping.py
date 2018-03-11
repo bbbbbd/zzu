b996f79128f39633b103b187e627ba09972d4601
bbbbbd <172655790@qq.com>
2018-03-11 15:27:17 +0800

    gevent

--- jiaoping.py
+++ jiaoping.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
+import gevent
+from gevent import monkey;monkey.patch_all()
+
 
 def post_save(url):
-    url = url
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
     try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
         post_url = soup.find('form')['action']
         result = soup.find_all('select')
         data = {}
         for item in result:
             data[item['name']] = item.find(attrs={'selected': ''}).string
         rep = requests.post(post_url, data)
-        rep.encoding = 'gbk'
         if rep.status_code == 200:
             print '教评完成'
     except:
@@ -22,27 +23,24 @@ def post_save(url):
 
 url = 'http://jw.zzu.edu.cn/scripts/jpxx/jpxxcgi.exe/check'
 data = {
-    'zhanghao':'*********',
-    'mima':'*********',
+    'zhanghao':'201577f0132',
+    'mima':'172655790',
     'shenfen':'stu'
 }
 response = requests.post(url, data=data)
-response.encoding = 'gbk'
 soup =BeautifulSoup(response.text, 'lxml')
 able = soup.find(color='green')
 if able:
     href = able.parent.attrs['href']
     response = requests.get(href)
-    response.encoding = 'gbk'
     soup = BeautifulSoup(response.text, 'lxml')
     result = soup.find_all(attrs={'width':'30%'})[2:]
     if result:
+        tasks = []
         for item in result:
             a = item.find('a')['href']
-            post_save(a)
+            tasks.append(gevent.spawn(post_save, a))
+        gevent.joinall(tasks)
 else:
     print '教评暂未开放'
-
-
-
 
